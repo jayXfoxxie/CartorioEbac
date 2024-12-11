fd6a064b2a5ed892a31ce5576774c1102e8b54d0
NicoleN++ <jayfoxxie00@gmail.com>
2024-12-11 19:08:33 -0300

    Add files via upload

---
+++ XCartório.c
@@ -0,0 +1,18 @@
+#include <stdio.h> //biblioteca de comunicação com o usuário
+#include <stdlib.h> //biblioteca de alocação de espaço em memória
+#include <locale.h> //biblioteca de alocações de texto por região
+
+int main()
+{
+
+	setlocale(LC_ALL, "Portuguese");
+	printf("### Cartório da EBAC ###\n\n");
+	printf("escolha a opção desejada do menu:\n\n");
+	printf("\t1 - Registrar nomes\n");
+	printf("\t2 - Consultar nomes\n");	
+	printf("\t3 - Deletar nomes\n\n");
+
+
+	printf("esse Softare e de livre uso dos alunos\n\n");
+
+	}
